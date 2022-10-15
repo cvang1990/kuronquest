3713d0dcba462ba27d90305a2b5615460cacefbf
cvang1990 <86810894+cvang1990@users.noreply.github.com>
2022-10-15 12:17:53 -0500

    Create kq-global.lua

---
+++ kq-global.lua
@@ -0,0 +1,17 @@
+ACTION_DECK = getObjectFromGUID('')
+BONUS_DECK = getObjectFromGUID('')
+QUEST_DECK = getObjectFromGUID('')
+QUEST_ITEM_DECK = getObjectFromGUID('')
+LEGENDARY_DECK = getObjectFromGUID('')
+
+QUEST_ZONE_ONE = Global.getVar('')
+QUEST_ZONE_TWO = Global.getVar('')
+QUEST_ZONE_THREE = Global.getVar('')
+
+function onLoad()
+
+end
+
+function
+
+end
