c7fdebf2e39d38a6776322c28dc8b557fb4e1d33
cvang1990 <86810894+cvang1990@users.noreply.github.com>
2022-10-16 19:03:52 -0500

    Update kq-global.lua

--- tts/kq-global.lua
+++ tts/kq-global.lua
@@ -1,18 +1,16 @@
-ACTION_DECK = getObjectFromGUID('')
-BONUS_DECK = getObjectFromGUID('')
-QUEST_DECK = getObjectFromGUID('')
-QUEST_ITEM_DECK = getObjectFromGUID('')
-LEGENDARY_DECK = getObjectFromGUID('')
+-- Constants in the game
+ACTION_DECK = "0cc996"
+BONUS_DECK = "c19164"
+QUEST_DECK = "f6bb8b"
 
-QUEST_ZONE_ONE = Global.getVar('')
-QUEST_ZONE_TWO = Global.getVar('')
-QUEST_ZONE_THREE = Global.getVar('')
+QUEST_ZONE_ONE = "e6e298"
+QUEST_ZONE_TWO = "55856e"
+QUEST_ZONE_THREE = "1e49cd"
 
+FINISH_SETUP = "false"
+
+-- When game starts
 function onLoad()
     broadcastToAll('Let the adventure begin!')
     broadcastToAll('Welcome to Kuron Quest.')
-end
-
-function
-
 end
